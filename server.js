8d20f3bb17b6a99fcd4ebf37871d9741cfee2de9
guimunizzz <guilherme.muniz5@hotmail.com>
2025-09-04 11:17:09 -0300

    Send exercise 2

--- server.js
+++ server.js
@@ -2,9 +2,36 @@ const app = require('express')();
 
 const PORT = 3006;
 
+async function calculadora(operacao, num1, num2) {
+  try {
+    if (operacao == soma) {
+      resultado = num1 + num2;
+    } else if (operacao == subtracao) {
+      resultado = num1 - num2;
+    } else if (operacao == multiplicacao) {
+      resultado = num1 * num2;
+    } else if (operacao == divisao) {
+      resultado = num1 / num2;
+    }
+    return resultado
+  } catch (error) {
+    throw new Error(`Erro ao ler o arquivo JSON: ${error} `);
+  }
+}
 
+app.get('/calculadora', async(req, res) => {
+  try {
+    const { operacao } = req.query;
+    const { num1 } = req.query;
+    const { num2 } = req.query;
 
-// start server on the port 3004
+    res.send()
+  } catch (error) {    
+    res.status(500).json({ erro: error.message});
+  }
+});
+
+// start server on the port 3006
 app.listen(PORT, () => {
   console.log(`Servidor executando na porta ${PORT}`);
 })
