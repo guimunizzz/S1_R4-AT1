f11581212c0a20843313445adb8daded44997a7a
guimunizzz <guilherme.muniz5@hotmail.com>
2025-09-18 08:04:13 -0300

    start exercise 3

--- server.js
+++ server.js
@@ -1,36 +1,7 @@
 const app = require('express')();
 
-const PORT = 3006;
+const PORT = 3008;
 
-function calculadora(operacao, num1, num2) {
-  if (operacao === "soma") {
-      return num1 + num2;
-    } else if (operacao === "subtracao") {
-      return num1 - num2;
-    } else if (operacao === "multiplicacao") {
-      return num1 * num2;
-    } else if (operacao === "divisao") {
-      if (num2 == 0) {
-        throw new Error("Divisão por zero não é permitida.");
-      }
-      return num1 / num2;
-    } else {
-      throw new Error('Operação inválida. Use "soma", "subtracao", "multiplicacao" ou "divisao".');
-    }
-}
-
-app.get('/calculadora', async(req, res) => {
-  try {
-    const { num1 ,num2 , operacao } = req.query;
-    const n1 = parseFloat(num1);
-    const n2 = parseFloat(num2);
-
-    resultadoFinal = calculadora(operacao, n1, n2);
-    res.status(200).send(`Resultado: ${resultadoFinal}`);
-  } catch (error) {    
-    res.status(400).send(`Erro: ${error.message}`);
-  }
-});
 
 // start server on the port 3006
 app.listen(PORT, () => {
