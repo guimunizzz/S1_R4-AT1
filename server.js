c9ed59d7e840b0fe49f71eb054d426ddf50d0808
guimunizzz <guilherme.muniz5@hotmail.com>
2025-09-18 09:08:42 -0300

    formatting code

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const app = require('express')();
+const app = require("express")();
 const PORT = 3012;
 
-
-app.get('/saudacao/:nome', async(req, res) => {
+app.get("/saudacao/:nome", async (req, res) => {
   try {
-    const nomeUsuario = (req.params.nome);
+    const nomeUsuario = req.params.nome;
     const { hora } = req.query;
     const horaDigitada = parseInt(hora);
 
@@ -15,18 +14,18 @@ app.get('/saudacao/:nome', async(req, res) => {
         res.send(`Bom dia, ${nomeUsuario}`);
       } else if (horaDigitada >= 12 && horaDigitada < 18) {
         res.send(`Boa tarde, ${nomeUsuario}`);
-      } else if (horaDigitada >= 18 && horaDigitada <= 23 || horaDigitada >= 0 && horaDigitada < 6) {
+      } else if ((horaDigitada >= 18 && horaDigitada <= 23) || (horaDigitada >= 0 && horaDigitada < 6)) {
         res.send(`Boa noite, ${nomeUsuario}`);
       } else {
         res.status(400).send("ERRO - digite uma hora válida!");
       }
     }
   } catch (error) {
-      res.status(500).json({ erro: error.message});
+    res.status(500).json({ erro: error.message });
   }
-})
+});
 
 // start server on the port 3012
 app.listen(PORT, () => {
   console.log(`Servidor executando na porta ${PORT}`);
-})
+});
