8e6115d703619b2bfa6251be5a4dcca95506fe65
guimunizzz <guilherme.muniz5@hotmail.com>
2025-09-18 08:54:29 -0300

    send exercise5

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const app = require('express')();
-const PORT = 3010;
+const PORT = 3012;
 
 
-app.get('/ano/:ano', async(req, res) => {
+app.get('/saudacao/:nome', async(req, res) => {
   try {
-    const anoDigitado = parseInt(req.params.ano);
+    const nomeUsuario = (req.params.nome);
+    const { hora } = req.query;
+    const horaDigitada = parseInt(hora);
 
-    if (isNaN(anoDigitado)) {
-      res.status(400).send("ERRO - digite um ano válido!");
+    if (isNaN(horaDigitada)) {
+      res.status(400).send("ERRO - digite uma numero válido!");
     } else {
-      if ((anoDigitado % 4 == 0) && (anoDigitado % 100 != 0) || (anoDigitado % 400 == 0)) {
-        res.send(`O ano ${anoDigitado} é bissexto!`);
+      if (horaDigitada >= 6 && horaDigitada < 12) {
+        res.send(`Bom dia, ${nomeUsuario}`);
+      } else if (horaDigitada >= 12 && horaDigitada < 18) {
+        res.send(`Boa tarde, ${nomeUsuario}`);
+      } else if (horaDigitada >= 18 && horaDigitada <= 23 || horaDigitada >= 0 && horaDigitada < 6) {
+        res.send(`Boa noite, ${nomeUsuario}`);
       } else {
-        res.send(`O ano ${anoDigitado} não é bissexto!`);
+        res.status(400).send("ERRO - digite uma hora válida!");
       }
     }
   } catch (error) {
@@ -20,7 +26,7 @@ app.get('/ano/:ano', async(req, res) => {
   }
 })
 
-// start server on the port 3010
+// start server on the port 3012
 app.listen(PORT, () => {
   console.log(`Servidor executando na porta ${PORT}`);
 })
